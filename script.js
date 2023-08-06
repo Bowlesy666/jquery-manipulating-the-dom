e43378da09004f7c643e64992082dbb3e54548f2
David Bowles <david.bowles84272@outlook.com>
2023-08-06 15:39:16 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+ 
+
+    $("#stream1_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+       $(".stream2").removeClass('highlight_stream');
+       $(".stream3").removeClass('highlight_stream');
+         $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+       $(".stream1").removeClass('highlight_stream');
+       $(".stream2").removeClass('highlight_stream');
+       $(".stream3").removeClass('highlight_stream');
+         $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+       $(".stream1").removeClass('highlight_stream');
+       $(".stream2").removeClass('highlight_stream');
+       $(".stream3").removeClass('highlight_stream');
+         $(".stream3").addClass('highlight_stream');
+   });
+
+
+}); 
+
+$('img');
+$('.card_image');
+$('#my-footer');
+$('#my-footer p');
+$('.header');
+$('div:first');
+$('div:last');
+$('img:last');
+$('a');
